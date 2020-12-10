3d04758a138b43629c112b71a1c68bac18f2128e
harshavardan25000-main <harsha.manam07@gmail.com>
2020-12-09 18:35:48 -0600

    without using inbuilt min max

--- Chapter1/r_1_3.py
+++ Chapter1/r_1_3.py
@@ -1,12 +1,14 @@
 def minmax(data):
-    if len(data)==1:
-        return (data[0],data[0])
-    min_1,max_1=data[0],data[0]
-    for i in range(1,len(data)):
-        min_1=min(min_1,data[i])
-        max_1=max(max_1,data[i])
+    if not data:
+        return None
+    min_1,max_1=float('inf'),float('-inf')
+    for i in data:
+        if i<min_1:
+            min_1=i
+        if i>max_1:
+            max_1=i
     return (min_1,max_1)
-data=[1]
+data=[1,2,3,4]
 
 if __name__=='__main__':
     result=minmax(data)
